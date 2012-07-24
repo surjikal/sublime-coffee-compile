ed7bf14fcb85a50d7394a8f833fcc3255624d790
Nicolas Porter <porter.nicolas@gmail.com>
2012-07-24 14:15:39 -0300

    Refactored and potentially added windows support.

--- coffee_compile.py
+++ coffee_compile.py
@@ -1,26 +1,38 @@
+import platform
+
 import sublime_plugin
 import sublime
+
+COFFEE_COMMAND = 'coffee.cmd' if platform.system() == 'Windows' else 'coffee'
 
 
 class CoffeeCompileCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        region = self.view.sel()[0] if self._buffer_contains_selected_text(self.view.sel()) \
-                 else sublime.Region(0, self.view.size())
-
-        text = self.view.substr(region)
+        text = self._get_text_to_compile()
         window = self.view.window()
 
         panel = window.get_output_panel("exec")
         panel.set_syntax_file('Packages/JavaScript/JavaScript.tmLanguage')
 
         window.run_command('exec', {
-            'cmd': ['coffee', '--print', '--lint', '--eval', text],
+            'cmd': [COFFEE_COMMAND, '--print', '--lint', '--eval', text],
             'quiet': True
         })
 
-    def _buffer_contains_selected_text(self, buffer):
-        for region in buffer:
+    def _get_text_to_compile(self):
+        region = self._get_selected_region() if self._editor_contains_selected_text() \
+            else self._get_region_for_entire_file()
+        return self.view.substr(region)
+
+    def _get_region_for_entire_file(self):
+        return sublime.Region(0, self.view.size())
+
+    def _get_selected_region(self):
+        return self.view.sel()[0]
+
+    def _editor_contains_selected_text(self):
+        for region in self.view.sel():
             if not region.empty():
                 return True
         return False
