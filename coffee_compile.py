fb7ee09e711c99627b101ffe8365070a78fe486b
Nicolas Porter <nporter@benbria.ca>
2012-08-21 15:59:12 -0400

    Removed old logging function and refactored a bit.

--- coffee_compile.py
+++ coffee_compile.py
@@ -2,27 +2,15 @@ import platform
 
 import sublime_plugin
 import sublime
-import os
 import subprocess
 
-
-PLATFORM_IS_WINDOWS = platform.system() == 'Windows'
-COFFEE_COMMAND = 'coffee.cmd' if PLATFORM_IS_WINDOWS else 'coffee'
-PANEL_NAME = 'coffeescript_output'
-
-
-def _log(msg):
-    print '[CoffeeCompile] %s' % msg
-
-_log('Platform is "%s"' % platform.system())
-_log('Coffee command is "%s"' % COFFEE_COMMAND)
-
-
-def is_null_empty(value):
-    return value is None or len(value) == 0
+PLATFORM_IS_WINDOWS = platform.system() is 'Windows'
 
 
 class CoffeeCompileCommand(sublime_plugin.TextCommand):
+
+    PANEL_NAME = 'coffeecompile_output'
+    COFFEE_COMMAND = 'coffee.cmd' if PLATFORM_IS_WINDOWS else 'coffee'
 
     def run(self, edit):
         text = self._get_text_to_compile()
@@ -32,26 +20,30 @@ class CoffeeCompileCommand(sublime_plugin.TextCommand):
         self._write_output_to_panel(window, javascript, error)
 
     def _compile(self, text, window):
-        args = [COFFEE_COMMAND, '--stdio', '--print', '--lint']
+        args = [self.COFFEE_COMMAND, '--stdio', '--print', '--lint']
+        startupinfo = self._get_startupinfo() if PLATFORM_IS_WINDOWS else None
 
-        process = subprocess.Popen(args, stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-            startupinfo=self.get_startupinfo())
+        process = subprocess.Popen(args,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            startupinfo=startupinfo)
+
         return process.communicate(text)
 
     def _write_output_to_panel(self, window, javascript, error):
-
-        panel = window.get_output_panel(PANEL_NAME)
+        panel = window.get_output_panel(self.PANEL_NAME)
         panel.set_syntax_file('Packages/JavaScript/JavaScript.tmLanguage')
 
-        output_view = window.get_output_panel(PANEL_NAME)
+        output_view = window.get_output_panel(self.PANEL_NAME)
         output_view.set_read_only(False)
         edit = output_view.begin_edit()
         output_view.insert(edit, 0, javascript)
         output_view.end_edit(edit)
         output_view.sel().clear()
         output_view.set_read_only(True)
-        window.run_command('show_panel', {'panel': 'output.' + PANEL_NAME})
+
+        window.run_command('show_panel', {'panel': 'output.%s' % self.PANEL_NAME})
 
     def _get_text_to_compile(self):
         region = self._get_selected_region() if self._editor_contains_selected_text() \
@@ -70,12 +62,7 @@ class CoffeeCompileCommand(sublime_plugin.TextCommand):
                 return True
         return False
 
-    def get_startupinfo(self):
-        info = None
-
-        if os.name == 'nt':
-            info = subprocess.STARTUPINFO()
-            info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-            info.wShowWindow = subprocess.SW_HIDE
-
-        return info
+    def _get_startupinfo(self):
+        info = subprocess.STARTUPINFO()
+        info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        info.wShowWindow = subprocess.SW_HIDE
