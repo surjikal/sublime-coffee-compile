f84de181346ebf1b35ebc1ca8d0d3a5c2b73a8f7
Nicolas Porter <porter.nicolas@gmail.com>
2013-07-31 15:37:47 -0400

    Added better error messages, refactored a bit.

--- coffee_compile.py
+++ coffee_compile.py
@@ -15,45 +15,69 @@ PLATFORM_IS_OSX     = PLATFORM is 'osx'
 IS_SUBLIME_TEXT_2 = int(sublime.version()) < 3000
 
 
+def log(msg):
+    sys.stdout.write("[CoffeeCompile] %s\n" % msg)
+
 
 class CoffeeCompileCommand(sublime_plugin.TextCommand):
 
     PANEL_NAME = 'coffeecompile_output'
-    DEFAULT_COFFEE_EXECUTABLE = 'coffee.cmd' if PLATFORM_IS_WINDOWS else 'coffee'
 
     def run(self, edit):
+        log("Platform is '%s'" % PLATFORM)
+
         self.settings = sublime.load_settings("CoffeeCompile.sublime-settings")
         self.window   = self.view.window()
+        self.editor   = SublimeTextEditorView(self.view)
 
-        coffeescript = self._get_text_to_compile()
+        coffeescript = self.editor.get_text()
         coffeescript = coffeescript.encode('utf8')
 
+        try:
+            javascript = self._compile(coffeescript)
+            self._write_javascript_to_panel(javascript, edit)
+        except CoffeeCompilationError as e:
+            self._write_compile_error_to_panel(e, edit)
+
+    def _compile(self, coffeescript):
         path = self._get_path()
         args = self._get_coffee_args()
 
-        self._log("Path is\n%s" % path)
-
-        try:
-            javascript = self._compile(coffeescript, args, path)
-            self._write_javascript_to_panel(javascript, edit)
-        except CoffeeCompileError as e:
-            self._log(e)
-            self._write_compile_error_to_panel(e, edit)
-
-    def _compile(self, coffeescript, args, path):
         try:
             javascript, error = self._execute_command(coffeescript, args, path)
-            if error: raise CoffeeCompilationError(error)
-            return javascript.decode('utf8')
+
+            if error:
+                raise CoffeeCompilationError(
+                    path
+                  , 'Something went horribly wrong  compiling your coffee'
+                  , error
+                )
+
+            if javascript:
+                javascript = javascript.strip()
+
+            if javascript == "env: node: No such file or directory":
+                message  = "`coffee` couldn't find NodeJS `coffee`.\n"
+                message += "Your `node_path` setting is probably not configured properly.\n\n"
+                message += "To configure CoffeeCompile, go to:\n"
+                message += "`Preferences > Package Settings > CoffeeCompile > Settings - User`"
+                raise CoffeeCompilationError(path, message, javascript)
+
+            return javascript
+
         except OSError as e:
-            raise CoffeeCompilationOSError(e, path)
+            raise CoffeeCompilationOSError(path, e)
 
     def _execute_command(self, text, args, path=None):
         # This is needed for Windows... not sure why. See:
         # https://github.com/surjikal/sublime-coffee-compile/issues/13
+
         if path and PLATFORM_IS_WINDOWS:
             os.environ['PATH'] = path
             path = None
+
+        log("Executing command:\nPath=%s\nargs=%s" % (path, args))
+
         env = {'PATH': path} if path else None
         process = subprocess.Popen(args,
             stdin=subprocess.PIPE,
@@ -61,7 +85,16 @@ class CoffeeCompileCommand(sublime_plugin.TextCommand):
             stderr=subprocess.STDOUT,
             startupinfo=self._get_startupinfo(),
             env=env)
-        return process.communicate(text)
+
+        output, error = process.communicate(text)
+
+        if output:
+            output = output.decode('utf8')
+
+        return (output, error)
+
+    def _create_panel(self):
+        return SublimeTextOutputPanel(self.window, self.PANEL_NAME)
 
     def _write_javascript_to_panel(self, javascript, edit):
         panel = self._create_panel()
@@ -70,35 +103,15 @@ class CoffeeCompileCommand(sublime_plugin.TextCommand):
 
     def _write_compile_error_to_panel(self, error, edit):
         panel = self._create_panel()
+        panel.set_syntax_file('Packages/Markdown/Markdown.tmLanguage')
         panel.display(str(error), edit)
-
-    def _create_panel(self):
-        return SublimeTextOutputPanel(self.window, self.PANEL_NAME)
-
-    def _get_text_to_compile(self):
-        region = self._get_region()
-        return self.view.substr(region)
-
-    def _get_region(self):
-        if self._editor_contains_selected_text():
-            # return selected region
-            return self.view.sel()[0]
-        else:
-            # return region that spans the entire file
-            return sublime.Region(0, self.view.size())
-
-    def _editor_contains_selected_text(self):
-        for region in self.view.sel():
-            if not region.empty():
-                return True
-        return False
 
     def _get_coffee_args(self):
         if self.settings.get('coffee_script_redux'):
-            self._log("Using coffee script redux.")
+            log("Using coffee script redux.")
             return self._get_redux_coffee_args()
         else:
-            self._log("Using vanilla compiler.")
+            log("Using vanilla compiler.")
             return self._get_vanilla_coffee_args()
 
     def _get_vanilla_coffee_args(self):
@@ -116,7 +129,13 @@ class CoffeeCompileCommand(sublime_plugin.TextCommand):
         return [coffee_executable, '--js']
 
     def _get_coffee_executable(self):
-        return self.settings.get('coffee_executable') or self.DEFAULT_COFFEE_EXECUTABLE
+        cmd = self.settings.get('coffee_executable')
+        if not cmd:
+            if PLATFORM_IS_WINDOWS:
+                cmd = 'coffee.cmd'
+            else:
+                cmd = 'coffee'
+        return cmd
 
     def _get_startupinfo(self):
         if PLATFORM_IS_WINDOWS:
@@ -147,10 +166,6 @@ class CoffeeCompileCommand(sublime_plugin.TextCommand):
 
         return coffee_path
 
-    def _log(self, msg):
-        sys.stdout.write("[CoffeeCompile] %s\n" % msg)
-
-
 
 class SublimeTextOutputPanel:
 
@@ -163,14 +178,13 @@ class SublimeTextOutputPanel:
         self._window.run_command('show_panel', {'panel': 'output.%s' % self.name})
 
     def write(self, text, edit=None):
-        if IS_SUBLIME_TEXT_2:
-            self._panel.set_read_only(False)
+        self._panel.set_read_only(False)
+        if IS_SUBLIME_TEXT_2 or PLATFORM_IS_WINDOWS:
             if not edit:
                 edit = self._panel.begin_edit()
             self._panel.insert(edit, 0, text)
             self._panel.end_edit(edit)
             self._panel.sel().clear()
-            self._panel.set_read_only(True)
         else:
             self._panel.run_command('append', {'characters': text})
 
@@ -185,34 +199,92 @@ class SublimeTextOutputPanel:
         try:
             return window.get_output_panel(name)
         except AttributeError:
+            log("Couldn't get output panel.")
             return window.create_output_panel(name)
 
 
 
-class CoffeeCompileError(Exception):
-    def __init__(self, message):
-        self.message
-    def __str__(self):
-        return str(self.message)
+class SublimeTextEditorView:
+    def __init__(self, view):
+        self._view = view
+
+    def get_text(self):
+        return self.get_selected_text() or self.get_all_text()
+
+    def has_selected_text(self):
+        for region in self._view.sel():
+            if not region.empty(): return True
+        return False
+
+    def get_selected_text(self):
+        if not self.has_selected_text():
+            return None
+        region = self._get_selected_region()
+        return self._view.substr(region)
+
+    def get_all_text(self):
+        region = self._get_full_region()
+        return self._view.substr(region)
+
+    def _get_selected_region(self):
+         return self._view.sel()[0]
+
+    def _get_full_region(self):
+        return sublime.Region(0, self._view.size())
 
 
 
 class CoffeeCompilationError(Exception):
-    """ Raised when your coffee is bad """
-    def __init__(self, message):
-        super(CoffeeCompilationError, self).__init__(message)
+    """ Raised when something went wrong with the subprocess call """
+    def __init__(self, path, message, details):
+        self.path = path
+        self.message = message
+        self.details = details
+
+    def __str__(self):
+        output = \
+"""# CoffeeCompile Error :(
+
+%(message)s
+
+## Details
+```
+%(details)s
+```
+
+## Halp
+If you're sure that you've configured the plugin properly,
+please open up an issue and I'll try to help you out!
+
+https://github.com/surjikal/sublime-coffee-compile/issues
+
+## Path
+```
+%(path)s
+```
+"""
+        return output % {
+            'message': self.message
+          , 'details': self.details
+          , 'path': "\n".join(self.path.split(':'))
+        }
 
 
 
-class CoffeeCompilationOSError(Exception):
-    """ Raised when your paths are not configured properly """
-    def __init__(self, error, path):
-        super(CoffeeCompilationOSError, self).__init__(self._make_message(error, path))
+class CoffeeCompilationOSError(CoffeeCompilationError):
 
-    def _make_message(self, error, path):
-        message = '# CoffeeCompile error\n'
-        if error.errno is 2:
-            message += 'Could not find your `coffee` executable\n'
-        message += "\n## Details\n%s\n\n" % str(error)
-        message += "## PATH\n%s\n" % "\n".join(path.split(':'))
-        return message
+    def __init__(self, path, osError):
+        message = "An OS Error was raised after calling `coffee`:\n"
+
+        if osError.errno is 2:
+            message  = "Could not find your `coffee` executable...\n"
+            message += "Your `coffee_path` setting is probably not configured properly.\n\n"
+            message += "To configure CoffeeCompile, go to:\n"
+            message += "`Preferences > Package Settings > CoffeeCompile > Settings - User`"
+
+
+        super(CoffeeCompilationOSError, self).__init__(
+            path=path,
+            message=message,
+            details=repr(osError)
+        )
